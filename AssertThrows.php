2e3086953d4b9653cdd4920004ee061acf20b53b
Gustavo Nieves <37160403+TavoNiievez@users.noreply.github.com>
2020-10-02 06:37:41 -0500

    Added assertDoesNotThrow* assertions (and more) (#11)
    * optimized code, added assertDoesNotThrow assertions, updated tests and phpunit versions.
    
    * Allow to test functions with parameters

--- AssertThrows.php
+++ AssertThrows.php
@@ -1,79 +1,178 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Codeception;
 
+use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\AssertionFailedError;
-use PHPUnit\Framework\TestCase;
+use Throwable;
 
 trait AssertThrows
 {
+    /**
+     * @param string|null $message
+     * @param Throwable $exception
+     */
+    private function checkException(?string $message, Throwable $exception)
+    {
+        $actualMessage = strtolower($exception->getMessage());
+
+        if (!$message || $message === $actualMessage) {
+            return;
+        }
+
+        throw new AssertionFailedError(
+            sprintf(
+                "Exception message '%s' was expected, but '%s' was received",
+                $message,
+                $actualMessage
+            )
+        );
+    }
 
     /**
      * Asserts that callback throws an exception
      *
      * @param $throws
-     * @param callable $fn
-     * @throws \Exception
+     * @param callable $func
+     * @param mixed ...$params
+     * @throws Throwable
      */
-    public function assertThrows($throws, callable $fn)
+    public function assertThrows($throws, callable $func, ...$params)
     {
-        $this->assertThrowsWithMessage($throws, false, $fn);
+        $this->assertThrowsWithMessage($throws, null, $func, $params);
     }
 
     /**
      * Asserts that callback throws an exception with a message
      *
-     * @param $throws
-     * @param $message
-     * @param callable $fn
+     * @param string|Throwable $throws
+     * @param string|null $message
+     * @param callable $func
+     * @param mixed ...$params
+     * @throws Throwable
      */
-    public function assertThrowsWithMessage($throws, $message, callable $fn)
+    public function assertThrowsWithMessage($throws, ?string $message, callable $func, ...$params)
     {
-        /** @var $this TestCase  * */
-        $result = $this->getTestResultObject();
-
-        if (is_array($throws)) {
-            $message = ($throws[1]) ? $throws[1] : false;
-            $throws = $throws[0];
+        if ($throws instanceof Throwable) {
+            $message = $throws->getMessage();
+            $throws = get_class($throws);
         }
 
-        if (is_string($message)) {
+        if ($message) {
             $message = strtolower($message);
         }
 
         try {
-            call_user_func($fn);
-        } catch (AssertionFailedError $e) {
-
-            if ($throws !== get_class($e)) {
-                throw $e;
-            }
-
-            if ($message !== false && $message !== strtolower($e->getMessage())) {
-                throw new AssertionFailedError("Exception message '$message' was expected, but '" . $e->getMessage() . "' was received");
-            }
-
-        } catch (\Exception $e) {
-            if ($throws) {
-                if ($throws !== get_class($e)) {
-                    throw new AssertionFailedError("Exception '$throws' was expected, but " . get_class($e) . " was thrown with message '" . $e->getMessage() . "'");
-                }
-
-                if ($message !== false && $message !== strtolower($e->getMessage())) {
-                    throw new AssertionFailedError("Exception message '$message' was expected, but '" . $e->getMessage() . "' was received");
-                }
+            if ($params) {
+                call_user_func_array($func, $params);
             } else {
-                throw $e;
+                call_user_func($func);
             }
+        } catch (AssertionFailedError $exception) {
+
+            if ($throws !== get_class($exception)) {
+                throw $exception;
+            }
+
+            $this->checkException($message, $exception);
+
+        } catch (Throwable $exception) {
+
+            if (!$throws) {
+                throw $exception;
+            }
+
+            $actualThrows = get_class($exception);
+
+            if ($throws !== $actualThrows) {
+                throw new AssertionFailedError(
+                    sprintf(
+                        "Exception '%s' was expected, but '%s' was thrown with message '%s'",
+                        $throws,
+                        get_class($exception),
+                        $exception->getMessage()
+                    )
+                );
+            }
+
+            $this->checkException($message, $exception);
         }
 
-        if ($throws) {
-            if (isset($e)) {
-                $this->assertTrue(true, 'exception handled');
-            } else {
-                throw new AssertionFailedError("Exception '$throws' was not thrown as expected");
-            }
+        if (!$throws) {
+            return;
         }
 
+        if (isset($exception)) {
+            Assert::assertTrue(true, 'Exception handled');
+            return;
+        }
+
+        throw new AssertionFailedError(
+            sprintf("Exception '%s' was not thrown as expected", $throws)
+        );
+    }
+
+    /**
+     * Asserts that callback does not throws an exception
+     *
+     * @param null|string|Throwable $throws
+     * @param callable $func
+     * @param mixed ...$params
+     */
+    public function assertDoesNotThrow($throws, callable $func, ...$params)
+    {
+        $this->assertDoesNotThrowWithMessage($throws, null, $func, $params);
+    }
+
+    /**
+     * Asserts that callback does not throws an exception with a message
+     *
+     * @param null|string|Throwable $throws
+     * @param string|null $message
+     * @param callable $func
+     * @param mixed ...$params
+     */
+    public function assertDoesNotThrowWithMessage($throws, ?string $message, callable $func, ...$params)
+    {
+        if ($throws instanceof Throwable) {
+            $message = $throws->getMessage();
+            $throws = get_class($throws);
+        }
+
+        try {
+            if ($params) {
+                call_user_func_array($func, $params);
+            } else {
+                call_user_func($func);
+            }
+        } catch (Throwable $exception) {
+            if (!$throws) {
+                throw new AssertionFailedError('Exception was not expected to be thrown');
+            }
+
+            $actualThrows = get_class($exception);
+
+            if ($throws != $actualThrows) {
+                Assert::assertNotSame($throws, $actualThrows);
+                return;
+            }
+
+            if (!$message) {
+                throw new AssertionFailedError(
+                    sprintf("Exception '%s' was not expected to be thrown", $throws)
+                );
+            }
+
+            $actualMessage = $exception->getMessage();
+
+            if ($message != $actualMessage) {
+                Assert::assertNotSame($message, $actualMessage);
+                return;
+            }
+
+            throw new AssertionFailedError(
+                sprintf("Exception '%s' with message '%s' was not expected to be thrown", $throws, $message)
+            );
+        }
     }
 }
--- Readme.md
+++ Readme.md
@@ -1,43 +1,12 @@
 # AssertThrows
 
+Handle exceptions inside a test without a stop! Works with PHPUnit and Codeception.
+
 [![Build Status](https://travis-ci.org/Codeception/AssertThrows.svg?branch=master)](https://travis-ci.org/Codeception/AssertThrows)
 
-Handle exceptions inside a test without a stop!
+## Installation
 
----
-
-Assertions for exceptions. Works with PHPUnit and Codeception.
-
-## Usage
-
-Catch exception thrown inside a code block.
-
-```php
-<?php
-$this->assertThrows(NotFoundException::class, function() {
-	$this->userController->show(999);
-});
-
-// alternatively
-$this->assertThrows(new NotFoundException, function() {
-	$this->userController->show(999);
-});
-?>
 ```
-
-You can optionally test the exception message:
-
-```php
-<?php
-$this->assertThrowsWithMessage(NotFoundException::class, 'my error message', function() {
-	throw new NotFoundException('my error message');
-});
-?>
-```
-
-### Installation
-
-```php
 composer require codeception/assert-throws --dev
 ```
 
@@ -45,11 +14,47 @@ Include `AssertThrows` trait it to a TestCase:
 
 ```php
 <?php
+
 class MyTest extends PHPUnit\Framework\TestCase
 {
     use Codeception\AssertThrows;
 
+    //...
 } 
 ```
 
-## License MIT
+## Usage
+
+Catch exception thrown inside a code block.
+
+```php
+<?php
+
+$this->assertThrows(NotFoundException::class, function() {
+	$this->userController->show(99);
+});
+
+// alternatively
+$this->assertThrows(new NotFoundException(), function() {
+	$this->userController->show(99);
+});
+
+// you can also assert that an exception is not throw
+$this->assertDoesNotThrow(NotFoundException::class, function() {
+    $this->userController->show(99);
+});
+```
+
+You can optionally test the exception message:
+
+```php
+<?php
+
+$this->assertThrowsWithMessage(
+    NotFoundException::class, 'my error message', function() {
+	throw new NotFoundException('my error message');
+    }
+);
+```
+
+### License MIT
